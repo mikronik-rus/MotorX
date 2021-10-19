352606fb09a94462fd272d1b47bdf0051cc2ed2d
Zalautdinov <50925258+Zalautdinov@users.noreply.github.com>
2021-10-19 07:48:43 +0300

    Add files via upload

---
+++ Motorx/motorx.cpp
@@ -0,0 +1,16 @@
+#include <motorx.h>
+
+/**
+ * @brief Создаем новый мотор 
+ * 
+ * @param chip тип микросхемы драйвера коллекторного мотора ( L9110=0, TA6586=1, VHN2SP30=2 )
+ * @param In1 1 порт входного сигнала для управления
+ * @param In2 2 порт входного сигнала для управления
+ */
+void MotorX::begin(byte chip, byte In1, byte In2)
+{
+}
+
+void MotorX::On()
+{
+}
